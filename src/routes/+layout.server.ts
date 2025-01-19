c66fd07ca9d05fc1739d2dae0391935ae09d3a3b
jgrove <26723005+jgrove2@users.noreply.github.com>
2025-01-19 23:33:01 +0000

    fix redirect

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,8 +3,10 @@ import { redirect} from "@sveltejs/kit";
 import type { LayoutServerLoad} from "./$types";
 
 export const load: LayoutServerLoad = async (event) => {
-	if (!event.locals.user) {
+	if (!event.locals.user && event.url.pathname !== "/login") {
 		return redirect(302, "/login");
+	} else if (event.locals.user && event.url.pathname === "/login") {
+		return redirect(302, "/");
 	}
 	let user = event.locals.user
 	return {
